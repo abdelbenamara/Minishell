28386586aa8cea18baa5ec2b9bc7ac945496672c
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-06 20:29:42 +0200

    Updated ft_here_document

--- ft_here_document.c
+++ ft_here_document.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 15:05:46 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/03 18:28:32 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/06 16:47:42 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,19 @@
 
 void	ft_here_document(char *limiter, int *writefd)
 {
-	const size_t	len = ft_strlen(limiter) + 2;
-	char			*eof;
+	const char		*warn = "here-document delimited by end-of-file";
+	const size_t	len = ft_strlen(limiter) + 1;
 	char			*str;
 
-	eof = ft_strjoin(limiter, "\n");
-	if (!eof)
-		return ;
-	str = get_next_line(STDIN_FILENO);
-	while (ft_strncmp(str, eof, len))
+	str = readline("> ");
+	while (str && ft_strncmp(str, limiter, len))
 	{
 		ft_putstr_fd(str, writefd[1]);
+		ft_putstr_fd("\n", writefd[1]);
 		free(str);
-		str = get_next_line(STDIN_FILENO);
+		str = readline("> ");
 	}
-	free(eof);
+	if (!str)
+		ft_printf("warning: %s (wanted `%s')\n", warn, limiter);
 	free(str);
 }
