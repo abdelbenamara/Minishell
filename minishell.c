df2a88d7ea68d79984d4481538a681d92c0cb9e2
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-06 20:30:11 +0200

    Updated minishell

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 18:52:49 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/06 13:22:42 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/06 20:28:06 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,26 +36,23 @@ static char	*ft_get_prompt(t_list **env)
 	const char	*home = ft_env_get(env, "HOME");
 	size_t		len;
 	char		*cwd;
-	char		*tmp;
+	char		*str;
 	char		*prompt;
 
 	len = 0;
 	if (home)
 		len = ft_strlen(home);
 	cwd = getcwd(NULL, 0);
-	tmp = NULL;
-	if (home && !ft_strncmp(cwd, home, len))
-		tmp = ft_strjoin("~", cwd + len);
-	if (tmp)
-	{
-		free(cwd);
-		cwd = ft_strjoin(tmp, " $ \033[00m");
-		free(tmp);
-	}
+	str = ft_strdup(cwd);
+	if (len && !ft_strncmp(cwd, home, len))
+		(free(str), str = ft_strjoin("~", cwd + len));
+	free(cwd);
+	cwd = ft_strjoin(str, " $ \033[00m");
+	free(str);
 	if (ft_atoi(ft_env_get(env, "?")))
-		prompt = ft_strjoin(FAILURE "\033[00m " PROMPT, cwd);
+		prompt = ft_strjoin(FAILURE PROMPT, cwd);
 	else
-		prompt = ft_strjoin(SUCCESS "\033[00m " PROMPT, cwd);
+		prompt = ft_strjoin(SUCCESS PROMPT, cwd);
 	free(cwd);
 	return (prompt);
 }
@@ -84,6 +81,6 @@ int	main(int ac, char **av, char **ep)
 		free(code);
 	}
 	ft_lstclear(&env, &free);
-	free(line);
-	return (ft_printf("exit\n"), 0);
+	ft_printf("exit\n");
+	return (EXIT_SUCCESS);
 }
