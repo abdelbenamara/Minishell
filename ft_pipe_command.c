7f40d0e719be5891c8023b9dd847fa1bf0fd9c3e
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-06 20:30:05 +0200

    Updated ft_pipe_command

--- ft_pipe_command.c
+++ ft_pipe_command.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 18:02:17 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/05 03:38:22 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/06 20:08:06 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,23 +25,24 @@ static uint8_t	ft_handle_input(t_list **cmds, int *writefd)
 	if (pipe(writefd) == -1)
 		return (perror("pipe"), 0);
 	if (here_doc)
-	{
-		ft_lst_pop(cmds, &free);
-		ft_here_document((*cmds)->content, writefd);
-		ft_lst_pop(cmds, &free);
-	}
+		(ft_lst_pop(cmds, &free), ft_here_document((*cmds)->content, writefd));
 	else
 	{
 		ft_lst_pop(cmds, &free);
-		ft_redirect_input((*cmds)->content, writefd);
-		ft_lst_pop(cmds, &free);
+		if (!ft_redirect_input((*cmds)->content, writefd))
+		{
+			while (*cmds && ft_strncmp((*cmds)->content, "|", 2))
+				ft_lst_pop(cmds, &free);
+			return (ft_lst_pop(cmds, &free), 0);
+		}
 	}
-	return (1);
+	return (ft_lst_pop(cmds, &free), 1);
 }
 
 static void	ft_child_do(t_list **cmds, t_list **env, int *writefd, int *readfd)
 {
 	int		code;
+	char	*cmd;
 
 	if (close(writefd[1]) == -1)
 		(perror("close"), exit(EXIT_FAILURE));
@@ -52,8 +53,12 @@ static void	ft_child_do(t_list **cmds, t_list **env, int *writefd, int *readfd)
 	if (dup2(readfd[1], STDOUT_FILENO) == -1)
 		(perror("dup2"), exit(EXIT_FAILURE));
 	code = EXIT_SUCCESS;
+	cmd = NULL;
 	if (*cmds)
-		code = ft_execute_command((*cmds)->content, env);
+		cmd = (*cmds)->content;
+	if (cmd && ft_strncmp(cmd, "|", 2)
+		&& ft_strncmp(cmd, ">>", 3) && ft_strncmp(cmd, ">", 2))
+		code = ft_execute_command(cmd, env);
 	if (close(writefd[0]) == -1)
 		(perror("close"), exit(EXIT_FAILURE));
 	if (close(readfd[1]) == -1)
@@ -63,36 +68,39 @@ static void	ft_child_do(t_list **cmds, t_list **env, int *writefd, int *readfd)
 	exit(code);
 }
 
-static void	ft_handle_output(t_list **cmds, int *readfd)
+static uint8_t	ft_handle_output(t_list **cmds, int *readfd, int fd)
 {
 	char	*str;
 
-	if (!(*cmds))
+	if (!(*cmds) || !ft_strncmp((*cmds)->content, "|", 2))
 	{
 		str = get_next_line(readfd[0]);
 		while (str)
-			(ft_putstr_fd(str, STDOUT_FILENO), \
-				free(str), str = get_next_line(readfd[0]));
+			(ft_putstr_fd(str, fd), free(str), str = get_next_line(readfd[0]));
 		if (close(readfd[0]) == -1)
-			return (perror("close"));
+			return (perror("close"), 0);
 	}
 	else if (!ft_strncmp((*cmds)->content, ">>", 3))
 	{
 		ft_lst_pop(cmds, &free);
-		ft_redirect_output((*cmds)->content, O_APPEND, readfd);
+		if (!ft_redirect_output((*cmds)->content, O_APPEND, readfd))
+			return (ft_lst_pop(cmds, &free), 0);
 		ft_lst_pop(cmds, &free);
 	}
 	else if (!ft_strncmp((*cmds)->content, ">", 2))
 	{
 		ft_lst_pop(cmds, &free);
-		ft_redirect_output((*cmds)->content, O_TRUNC, readfd);
+		if (!ft_redirect_output((*cmds)->content, O_TRUNC, readfd))
+			return (ft_lst_pop(cmds, &free), 0);
 		ft_lst_pop(cmds, &free);
 	}
+	return (1);
 }
 
 static uint8_t	ft_parent_do(t_list **cmds, int *writefd, int *readfd)
 {
 	char	*str;
+	int		fd;
 
 	if (close(writefd[0]) == -1)
 		return (perror("close"), 0);
@@ -101,20 +109,20 @@ static uint8_t	ft_parent_do(t_list **cmds, int *writefd, int *readfd)
 	if (!(*cmds))
 		return (1);
 	str = (*cmds)->content;
-	if (ft_strncmp(str, ">>", 3) && ft_strncmp(str, ">", 2))
+	if (ft_strncmp(str, "|", 2)
+		&& ft_strncmp(str, ">>", 3) && ft_strncmp(str, ">", 2))
 		ft_lst_pop(cmds, &free);
-	ft_handle_output(cmds, readfd);
-	if (*cmds)
+	fd = STDOUT_FILENO;
+	if (*cmds && !ft_strncmp((*cmds)->content, "|", 2))
 	{
 		if (pipe(writefd) == -1)
 			return (perror("pipe"), 0);
-		str = get_next_line(readfd[0]);
-		while (str)
-			(ft_putstr_fd(str, writefd[1]), \
-				free(str), str = get_next_line(readfd[0]));
-		if (close(readfd[0]) == -1)
-			return (perror("close"), 0);
+		fd = writefd[1];
 	}
+	if (!ft_handle_output(cmds, readfd, fd))
+		return (0);
+	if (*cmds && !ft_strncmp((*cmds)->content, "|", 2))
+		ft_lst_pop(cmds, &free);
 	return (1);
 }
 
