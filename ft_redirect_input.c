07545e57ded1fadc1e2fcfe3460bfc22182f321d
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-06 20:29:46 +0200

    Updated ft_redirect_input

--- ft_redirect_input.c
+++ ft_redirect_input.c
@@ -6,13 +6,13 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 15:04:18 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/02 20:39:23 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/06 17:59:12 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_redirect_input(char *filename, int *writefd)
+uint8_t	ft_redirect_input(char *filename, int *writefd)
 {
 	int		fd;
 	char	*str;
@@ -20,7 +20,7 @@ void	ft_redirect_input(char *filename, int *writefd)
 
 	fd = open(filename, O_RDONLY | O_NONBLOCK);
 	if (fd == -1)
-		return (perror(filename));
+		return (perror(filename), 0);
 	str = get_next_line(fd);
 	dlines = 0;
 	if (!ft_strncmp(filename, "/dev/", 5))
@@ -31,5 +31,6 @@ void	ft_redirect_input(char *filename, int *writefd)
 	while (str)
 		(ft_putstr_fd(str, writefd[1]), free(str), str = get_next_line(fd));
 	if (close(fd) == -1)
-		return (perror(filename));
+		return (perror(filename), 0);
+	return (1);
 }
