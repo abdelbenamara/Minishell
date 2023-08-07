1ce89d934238334c363f5741569d5aadfe5d3e6b
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-07 18:25:11 +0200

    Updated ft_redirect_input

--- ft_redirect_input.c
+++ ft_redirect_input.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 15:04:18 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/06 17:59:12 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/07 18:17:37 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,27 @@
 
 uint8_t	ft_redirect_input(char *filename, int *writefd)
 {
-	int		fd;
-	char	*str;
-	size_t	dlines;
+	const uint8_t	device = !ft_strncmp(filename, "/dev/", 5);
+	int				fd;
+	char			*str;
+	size_t			dlines;
 
-	fd = open(filename, O_RDONLY | O_NONBLOCK);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (perror(filename), 0);
 	str = get_next_line(fd);
 	dlines = 0;
-	if (!ft_strncmp(filename, "/dev/", 5))
-		while (++dlines <= DLINES_MAX && str)
-			(ft_putstr_fd(str, writefd[1]), free(str), str = get_next_line(fd));
-	if (str && dlines)
-		(free(str), str = NULL);
-	while (str)
-		(ft_putstr_fd(str, writefd[1]), free(str), str = get_next_line(fd));
+	while (dlines < DLINES_MAX && str)
+	{
+		ft_putstr_fd(str, writefd[1]);
+		free(str);
+		str = get_next_line(fd);
+		if (device && str && str[ft_strlen(str) - 1] == '\n')
+			++dlines;
+	}
 	if (close(fd) == -1)
-		return (perror(filename), 0);
-	return (1);
+		return (perror(filename), free(str), 0);
+	if (g_signum == SIGINT)
+		return (ft_printf("\n"), free(str), 0);
+	return (free(str), 1);
 }
