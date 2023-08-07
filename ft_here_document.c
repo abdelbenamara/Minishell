8e4c491dcaa89a344db104854326074604a55bae
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-07 18:25:08 +0200

    Updated ft_here_document

--- ft_here_document.c
+++ ft_here_document.c
@@ -6,13 +6,13 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 15:05:46 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/06 16:47:42 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/07 16:14:31 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_here_document(char *limiter, int *writefd)
+uint8_t	ft_here_document(char *limiter, int *writefd)
 {
 	const char		*warn = "here-document delimited by end-of-file";
 	const size_t	len = ft_strlen(limiter) + 1;
@@ -21,6 +21,8 @@ void	ft_here_document(char *limiter, int *writefd)
 	str = readline("> ");
 	while (str && ft_strncmp(str, limiter, len))
 	{
+		if (g_signum == SIGINT)
+			return (0);
 		ft_putstr_fd(str, writefd[1]);
 		ft_putstr_fd("\n", writefd[1]);
 		free(str);
@@ -29,4 +31,5 @@ void	ft_here_document(char *limiter, int *writefd)
 	if (!str)
 		ft_printf("warning: %s (wanted `%s')\n", warn, limiter);
 	free(str);
+	return (1);
 }
