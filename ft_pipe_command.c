1f8348c8298916a6ecebf6cc832f5280c55a0874
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-07 21:38:10 +0200

    Updated ft_pipe_command

--- ft_pipe_command.c
+++ ft_pipe_command.c
@@ -6,37 +6,39 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 18:02:17 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/07 18:16:41 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/07 20:36:46 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static uint8_t	ft_handle_input(t_list **cmds, int *writefd)
+static int	ft_handle_input(t_list **cmds, int *writefd)
 {
 	const uint8_t	here_doc = !ft_strncmp((*cmds)->content, "<<", 3);
 
 	if (!here_doc && ft_strncmp((*cmds)->content, "<", 2))
 		return (1);
 	if (close(writefd[1]) == -1)
-		return (perror("close"), 0);
+		return (perror("close"), -1);
 	if (close(writefd[0]) == -1)
-		return (perror("close"), 0);
+		return (perror("close"), -1);
 	if (pipe(writefd) == -1)
-		return (perror("pipe"), 0);
+		return (perror("pipe"), -1);
 	ft_lst_pop(cmds, &free);
+	if (!(*cmds) || !ft_strncmp((*cmds)->content, "|", 2))
+		return (ft_printf("syntax error near unexpected token `newline'\n"), -2);
 	if (here_doc)
 	{
 		if (!ft_here_document((*cmds)->content, writefd))
-			return (ft_lstclear(cmds, &free), 0);
+			return (ft_lstclear(cmds, &free), -1);
 	}
 	else if (!ft_redirect_input((*cmds)->content, writefd))
 	{
 		while (*cmds && ft_strncmp((*cmds)->content, "|", 2))
 			ft_lst_pop(cmds, &free);
-		return (ft_lst_pop(cmds, &free), 0);
+		return (ft_lst_pop(cmds, &free), -1);
 	}
-	return (ft_lst_pop(cmds, &free), 1);
+	return (ft_lst_pop(cmds, &free), EXIT_SUCCESS);
 }
 
 static void	ft_child_do(t_list **cmds, t_list **env, int *writefd, int *readfd)
@@ -67,7 +69,7 @@ static void	ft_child_do(t_list **cmds, t_list **env, int *writefd, int *readfd)
 	(lc(cmds, &free), lc(env, &free), exit(code));
 }
 
-static uint8_t	ft_handle_output(t_list **cmds, int *readfd, int fd)
+static int	ft_handle_output(t_list **cmds, int *readfd, int fd, int wstatus)
 {
 	char	*str;
 	int		openflag;
@@ -79,32 +81,33 @@ static uint8_t	ft_handle_output(t_list **cmds, int *readfd, int fd)
 		while (str)
 			(ft_putstr_fd(str, fd), free(str), str = get_next_line(readfd[0]));
 		if (close(readfd[0]) == -1)
-			return (perror("close"), 0);
-		return (1);
+			return (perror("close"), -1);
+		return (wstatus);
 	}
 	if (!ft_strncmp((*cmds)->content, ">>", 3))
 		openflag = O_APPEND;
 	else if (!ft_strncmp((*cmds)->content, ">", 2))
 		openflag = O_TRUNC;
 	else
-		return (1);
+		return (wstatus);
 	ft_lst_pop(cmds, &free);
+	if (!(*cmds) || !ft_strncmp((*cmds)->content, "|", 2))
+		return (ft_printf("syntax error near unexpected token `newline'\n"), -2);
 	if (!ft_redirect_output((*cmds)->content, openflag, readfd))
-		return (ft_lst_pop(cmds, &free), 0);
-	ft_lst_pop(cmds, &free);
-	return (1);
+		return (ft_lst_pop(cmds, &free), -1);
+	return (ft_lst_pop(cmds, &free), wstatus);
 }
 
-static uint8_t	ft_parent_do(t_list **cmds, int *writefd, int *readfd)
+static int	ft_parent_do(t_list **cmds, int *writefd, int *readfd, int wstatus)
 {
 	char	*str;
 
 	if (close(writefd[0]) == -1)
-		return (perror("close"), 0);
+		return (perror("close"), -1);
 	if (close(readfd[1]) == -1)
-		return (perror("close"), 0);
+		return (perror("close"), -1);
 	if (!(*cmds))
-		return (1);
+		return (wstatus);
 	str = (*cmds)->content;
 	if (ft_strncmp(str, "|", 2)
 		&& ft_strncmp(str, ">>", 3) && ft_strncmp(str, ">", 2))
@@ -112,10 +115,10 @@ static uint8_t	ft_parent_do(t_list **cmds, int *writefd, int *readfd)
 	if (*cmds && !ft_strncmp((*cmds)->content, "|", 2))
 	{
 		if (pipe(writefd) == -1)
-			return (perror("pipe"), 0);
-		return (ft_handle_output(cmds, readfd, writefd[1]));
+			return (perror("pipe"), -1);
+		return (ft_handle_output(cmds, readfd, writefd[1], wstatus));
 	}
-	return (ft_handle_output(cmds, readfd, STDOUT_FILENO));
+	return (ft_handle_output(cmds, readfd, STDOUT_FILENO, wstatus));
 }
 
 int	ft_pipe_command(t_list **cmds, t_list **env, int *writefd, int *readfd)
@@ -125,23 +128,22 @@ int	ft_pipe_command(t_list **cmds, t_list **env, int *writefd, int *readfd)
 
 	if (pipe(readfd) == -1)
 		return (perror("pipe"), -1);
-	if (!ft_handle_input(cmds, writefd))
-		return (-1);
+	wstatus = ft_handle_input(cmds, writefd);
+	if (wstatus < 0)
+		return (wstatus);
 	cpid = fork();
 	if (cpid == -1)
 		return (perror("fork"), -1);
 	if (!cpid)
-		ft_child_do(cmds, env, writefd, readfd);
-	else
-	{
-		if (close(writefd[1]) == -1)
-			return (perror("close"), -1);
-		if (waitpid(cpid, &wstatus, 0) == -1)
-			return (perror((*cmds)->content), -1);
-		if (!ft_parent_do(cmds, writefd, readfd))
-			return (-1);
-		if (*cmds)
-			wstatus = ft_pipe_command(cmds, env, writefd, readfd);
-	}
+		return (ft_child_do(cmds, env, writefd, readfd), 0);
+	if (close(writefd[1]) == -1)
+		return (perror("close"), -1);
+	if (waitpid(cpid, &wstatus, 0) == -1)
+		return (perror((*cmds)->content), -1);
+	wstatus = ft_parent_do(cmds, writefd, readfd, wstatus);
+	if (wstatus < 0)
+		return (wstatus);
+	if (*cmds)
+		wstatus = ft_pipe_command(cmds, env, writefd, readfd);
 	return (wstatus);
 }
