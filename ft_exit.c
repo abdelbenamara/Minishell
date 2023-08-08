ed25b6361789d7b5bb8dd7b7c7ee4e99eede5f16
Anes BENAMARA <abenamar@student.42.fr>
2023-08-08 19:51:36 +0200

    Updated ft_exit

--- ft_exit.c
+++ ft_exit.c
@@ -6,13 +6,13 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 22:38:20 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/08 04:22:55 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/08 15:33:28 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_exit(char *cmd, char **argv, t_list **env)
+int	ft_exit(char **argv, t_list **env)
 {
 	size_t	i;
 
@@ -20,9 +20,9 @@ int	ft_exit(char *cmd, char **argv, t_list **env)
 	while (argv[i])
 		++i;
 	if (i > 2)
-		return (ft_dprintf(STDERR_FILENO, "exit: too many arguments\n"), \
-			ft_free_tab(argv), EXIT_FAILURE);
-	((void) cmd, g_signum = SIGTERM);
+		return (ft_dprintf(STDERR_FILENO, \
+			"exit: too many arguments\n"), EXIT_FAILURE);
+	g_signum = SIGTERM;
 	if (argv[1])
 	{
 		i = 0;
@@ -32,11 +32,11 @@ int	ft_exit(char *cmd, char **argv, t_list **env)
 			{
 				ft_dprintf(STDERR_FILENO, \
 					"exit: %s: numeric argument required\n", argv[1]);
-				return (2 - 256);
+				return (2);
 			}
 			++i;
 		}
-		return (ft_atoi(argv[1]) - 256);
+		return (ft_atoi(argv[1]));
 	}
-	return (ft_atoi(ft_env_gets(env, "?")) - 256);
+	return (ft_env_geti(env, "?"));
 }
