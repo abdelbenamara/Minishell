6d06fd6fe8732bbe2e66547ac79de9604a9265c6
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-08 04:59:08 +0200

    Added ft_parse_arguments

---
+++ ft_parse_arguments.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parse_arguments.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/07 23:10:30 by abenamar          #+#    #+#             */
+/*   Updated: 2023/08/07 23:15:53 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static char	*ft_setup_command(char *cmd)
+{
+	size_t	i;
+	size_t	j;
+	char	c;
+
+	i = 0;
+	while (cmd[i])
+	{
+		if (cmd[i] == '\'' || cmd[i] == '"')
+		{
+			c = cmd[i];
+			j = i + 1;
+			while (cmd[j] && cmd[j] != c)
+				++j;
+			if (c == cmd[j])
+			{
+				cmd[i] = ' ';
+				while ((++i) < j)
+					if (cmd[i] == ' ')
+						cmd[i] = '\n';
+				cmd[j] = ' ';
+			}
+		}
+		++i;
+	}
+	return (cmd);
+}
+
+char	**ft_parse_arguments(char *cmd)
+{
+	char	**argv;
+	size_t	i;
+	size_t	j;
+
+	argv = ft_split(ft_setup_command(cmd), ' ');
+	if (!argv)
+		return (NULL);
+	i = 0;
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] == '\n')
+				argv[i][j] = ' ';
+			++j;
+		}
+		++i;
+	}
+	return (argv);
+}
