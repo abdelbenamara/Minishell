bade20addd786b78ba4c7f1b6946681ff05eabc4
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-08 04:56:46 +0200

    Updated ft_env_gets

--- ft_env_gets.c
+++ ft_env_gets.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_env_get.c                                       :+:      :+:    :+:   */
+/*   ft_env_gets.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 01:55:06 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/05 04:59:57 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/08 02:00:30 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*ft_env_get(t_list **env, char *key)
+char	*ft_env_gets(t_list **env, char *key)
 {
 	size_t	len;
 	t_list	*lst;
