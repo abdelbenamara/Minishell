b2c368f7d8b1926344f6ac0d6f5ee7ce03dc4a1c
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-08 04:57:04 +0200

    Updated ft_env_puts

--- ft_env_puts.c
+++ ft_env_puts.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_env_put.c                                       :+:      :+:    :+:   */
+/*   ft_env_puts.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 04:04:45 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/05 04:45:31 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/08 02:00:23 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_env_put(t_list **env, char *key, char *value)
+void	ft_env_puts(t_list **env, char *key, char *value)
 {
 	char	*str;
 	char	*var;
@@ -26,8 +26,8 @@ void	ft_env_put(t_list **env, char *key, char *value)
 	free(var);
 	if (!str)
 		return ;
-	var = ft_env_get(env, key);
+	var = ft_env_gets(env, key);
 	if (var)
 		ft_lst_pop(env, &free);
-	return (ft_lstadd_front(env, ft_lstnew(str)));
+	ft_lstadd_front(env, ft_lstnew(str));
 }
