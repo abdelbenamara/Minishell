75ea32c04b5c6648e37e6b45743a677fe7e2a047
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-08 04:59:57 +0200

    Updated minishell

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 18:52:49 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/07 20:32:18 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/08 02:32:11 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	g_signum = 0;
 
-static void	ft_rl_redisplay(int signum)
+static void	ft_handle_signal(int signum)
 {
 	g_signum = signum;
 	if (signum == SIGINT)
@@ -23,21 +23,21 @@ static void	ft_rl_redisplay(int signum)
 
 static void	ft_setup_sigaction(void)
 {
-	struct sigaction	int_act;
-	struct sigaction	quit_act;
+	struct sigaction	act;
+	struct sigaction	no_act;
 
 	rl_event_hook = &ft_event_hook;
-	int_act.sa_handler = &ft_rl_redisplay;
-	int_act.sa_flags = 0;
-	if (sigemptyset(&(int_act.sa_mask)) == -1)
-		perror("SIGINT sigemptyset");
-	if (sigaction(SIGINT, &int_act, NULL) == -1)
+	act.sa_handler = &ft_handle_signal;
+	act.sa_flags = 0;
+	if (sigemptyset(&(act.sa_mask)) == -1)
+		perror("act sigemptyset");
+	if (sigaction(SIGINT, &act, NULL) == -1)
 		perror("SIGINT sigaction");
-	quit_act.sa_handler = SIG_IGN;
-	quit_act.sa_flags = 0;
-	if (sigemptyset(&(quit_act.sa_mask)) == -1)
-		perror("SIGQUIT sigemptyset");
-	if (sigaction(SIGQUIT, &quit_act, NULL) == -1)
+	no_act.sa_handler = SIG_IGN;
+	no_act.sa_flags = 0;
+	if (sigemptyset(&(no_act.sa_mask)) == -1)
+		perror("no_act sigemptyset");
+	if (sigaction(SIGQUIT, &no_act, NULL) == -1)
 		perror("SIGQUIT sigaction");
 }
 
@@ -56,13 +56,13 @@ static t_list	*ft_init_env(char **ep)
 			ft_lstadd_front(&lst, ft_lstnew(var));
 		++i;
 	}
-	ft_env_put(&lst, "?", "0");
+	ft_env_puts(&lst, "?", "0");
 	return (lst);
 }
 
 static char	*ft_get_prompt(t_list **env)
 {
-	const char	*home = ft_env_get(env, "HOME");
+	const char	*home = ft_env_gets(env, "HOME");
 	size_t		len;
 	char		*cwd;
 	char		*str;
@@ -81,7 +81,7 @@ static char	*ft_get_prompt(t_list **env)
 	free(cwd);
 	cwd = ft_strjoin(str, " $ \033[00m");
 	free(str);
-	if (ft_atoi(ft_env_get(env, "?")))
+	if (ft_atoi(ft_env_gets(env, "?")))
 		prompt = ft_strjoin(FAILURE PROMPT, cwd);
 	else
 		prompt = ft_strjoin(SUCCESS PROMPT, cwd);
@@ -92,26 +92,27 @@ static char	*ft_get_prompt(t_list **env)
 int	main(int ac, char **av, char **ep)
 {
 	t_list	*env;
+	char	*str;
 	char	*line;
-	char	*code;
-	char	*prompt;
+	int		code;
 
-	((void) ac, (void) av);
-	ft_setup_sigaction();
-	env = ft_init_env(ep);
+	((void) ac, (void) av, ft_setup_sigaction(), env = ft_init_env(ep));
 	line = ft_strdup("");
+	code = 0;
 	while (line)
 	{
-		g_signum = 0;
-		free(line);
-		prompt = ft_get_prompt(&env);
-		line = readline(prompt);
-		free(prompt);
+		(free(line), str = ft_get_prompt(&env));
+		line = readline(str);
+		free(str);
 		if (!g_signum && line && *line)
-			(add_history(line), code = ft_itoa(ft_process_line(line, &env)), \
-				ft_env_put(&env, "?", code), free(code));
+			(add_history(line), code = ft_process_line(line, &env), \
+				ft_env_puti(&env, "?", code));
+		if (g_signum == SIGTERM)
+			return (ft_lstclear(&env, &free), ft_printf("exit\n"), \
+				(-1 * code) + 256);
 		if (g_signum == SIGINT)
-			ft_env_put(&env, "?", "130");
+			(ft_env_puts(&env, "?", "130"), g_signum = 0);
 	}
-	return (ft_lstclear(&env, &free), ft_printf("exit\n"), EXIT_SUCCESS);
+	code = ft_atoi(ft_env_gets(&env, "?"));
+	return (ft_lstclear(&env, &free), ft_printf("exit\n"), code);
 }
