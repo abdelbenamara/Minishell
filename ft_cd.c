08bf616069d7d4d378231b3ec4a5cd4cdb59f104
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-08 04:58:24 +0200

    Added ft_cd

---
+++ ft_cd.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cd.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/08 04:46:35 by abenamar          #+#    #+#             */
+/*   Updated: 2023/08/08 04:48:17 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	ft_cd(char *cmd, char **argv, t_list **env)
+{
+	(void) cmd;
+	(void) argv;
+	(void) env;
+	return (ft_printf("TODO\n"), EXIT_SUCCESS);
+}
