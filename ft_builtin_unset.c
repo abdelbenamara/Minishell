f3969290616e690b3d1ee82dfb392050b75ef1c8
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-12 01:35:32 +0200

    Updated ft_builtin_unset

--- ft_builtin_unset.c
+++ ft_builtin_unset.c
@@ -1,26 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_unset.c                                         :+:      :+:    :+:   */
+/*   ft_builtin_unset.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 04:20:23 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/08 04:42:23 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/11 10:32:32 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_unset(char *cmd, char **argv, t_list **env)
+int	ft_builtin_unset(char **argv, t_list **env)
 {
 	size_t	i;
 	char	*var;
 
 	if (argv[1] && !ft_strncmp(argv[1], "-", 1))
-		return (ft_dprintf(STDERR_FILENO, \
-			"unset: %s: invalid option\n", argv[1]), -2);
-	(void) cmd;
+		return (ft_pstderr3("unset", argv[1], "invalid option"), 2);
 	i = 1;
 	while (argv[i])
 	{
