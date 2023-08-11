00680d3ba7188cd18a8b41eeda15ebea20fc21a1
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-12 01:35:38 +0200

    Added ft_check_export_identifier

---
+++ ft_check_export_identifier.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_export_identifier.c                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/11 14:21:13 by abenamar          #+#    #+#             */
+/*   Updated: 2023/08/11 14:22:19 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+uint8_t	ft_check_export_identifier(char *str)
+{
+	size_t	i;
+
+	if (!ft_isalpha(str[0]) && str[0] != '_')
+		return (0);
+	i = 0;
+	while (str[i] && (ft_isalnum(str[i]) || str[0] == '_'))
+		++i;
+	if (str[i] && str[i] != '=')
+		return (0);
+	str[i] = '\0';
+	return (1);
+}
