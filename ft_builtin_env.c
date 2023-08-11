e599131bbe305bad1034b5c6543bb84c5e2049ee
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-12 01:34:12 +0200

    Updated ft_builtin_env

--- ft_builtin_env.c
+++ ft_builtin_env.c
@@ -1,32 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_env.c                                           :+:      :+:    :+:   */
+/*   ft_builtin_env.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 01:11:01 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/08 18:32:18 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/12 01:22:46 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_env(char **argv, t_list **env)
+int	ft_builtin_env(char **argv, t_list **env)
 {
 	t_list	*lst;
 
 	if (!env)
 		return (EXIT_FAILURE);
 	if (argv[1])
-		return (ft_dprintf(STDERR_FILENO, \
-			"env: %s: invalid option / argument\n", argv[1]), 125);
+		return (ft_pstderr3("env", argv[1], "invalid option / argument"), 125);
 	lst = *env;
 	while (lst)
 	{
 		if (ft_strncmp(lst->content, "?=", 2)
-			&& ft_strncmp(lst->content, "|=", 2))
-			ft_printf("%s\n", lst->content);
+			&& ft_strncmp(lst->content, "!pipe=", 6)
+			&& ft_strncmp(lst->content, "!exit=", 6))
+		{
+			if (*(ft_strchr(((char *) lst->content), '=') + 1))
+				ft_printf("%s\n", lst->content);
+		}
 		lst = lst->next;
 	}
 	return (EXIT_SUCCESS);
