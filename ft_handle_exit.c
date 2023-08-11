559ca7ebc31ed63c891ad9ab838ea1d5bc980766
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-12 01:36:17 +0200

    Updated ft_handle_exit

--- ft_handle_exit.c
+++ ft_handle_exit.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 14:41:14 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/08 18:26:29 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/12 01:19:28 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,25 +25,33 @@ static void	ft_env_put_pipe_counts(t_list **cmds, t_list **env)
 			++count;
 		lst = lst->next;
 	}
-	ft_env_puti(env, "|", count);
+	ft_env_puti(env, "!pipe", count);
 }
 
+/*
+	TODO : check the arguments of the exit command as in ft_builtin_exit
+*/
 int	ft_handle_exit(t_list **cmds, t_list **env, int wstatus)
 {
 	char	**argv;
-	int		code;
 
-	if (!(*cmds) || (ft_env_gets(env, "|") && ft_env_geti(env, "|")))
+	if (!(*cmds) || (ft_env_gets(env, "!pipe") && ft_env_geti(env, "!pipe")))
 		return (wstatus);
-	if (!ft_env_gets(env, "|"))
+	if (!ft_env_gets(env, "!pipe"))
 		ft_env_put_pipe_counts(cmds, env);
-	if (ft_env_geti(env, "|"))
+	if (ft_env_geti(env, "!pipe"))
 		return (wstatus);
 	argv = ft_parse_arguments((*cmds)->content);
 	if (!argv)
 		return (-1);
-	code = wstatus;
 	if (!ft_strncmp("exit", argv[0], 5))
-		return (code = ft_exit(argv, env), ft_free_tab(argv), code - 512);
-	return (ft_free_tab(argv), code);
+	{
+		if (ft_env_geti(env, "SHLVL") == ft_env_geti(env, "!exit"))
+			(ft_env_puti(env, "SHLVL", ft_env_geti(env, "SHLVL") - 1), \
+				g_signum = SIGTERM);
+		else
+			(ft_env_puti(env, "SHLVL", ft_env_geti(env, "SHLVL") - 1), \
+				ft_printf("exit\n"));
+	}
+	return (ft_free_tab(argv), wstatus);
 }
