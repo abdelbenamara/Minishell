cefccbf4dd8fa408b8dc43f7f4b7f8e3f357acd5
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-12 01:35:22 +0200

    Updated ft_builtin_skip

--- ft_builtin_skip.c
+++ ft_builtin_skip.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_pass.c                                          :+:      :+:    :+:   */
+/*   ft_builtin_skip.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 02:14:32 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/08 04:50:01 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/09 18:31:29 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_pass(char **argv, t_list **env)
+int	ft_builtin_skip(char **argv, t_list **env)
 {
 	(void) argv;
 	(void) env;
