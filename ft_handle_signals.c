ca20d9ac7d024de146f58b979d4a305abdd6108e
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-12 01:37:05 +0200

    Updated ft_handle_signals

--- ft_handle_signals.c
+++ ft_handle_signals.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 17:11:43 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/08 17:43:34 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/10 17:50:32 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ static void	ft_handle_sigint(int signum)
 		rl_done = 1;
 }
 
-void	ft_handle_signals(void)
+uint8_t	ft_handle_signals(void)
 {
 	struct sigaction	act;
 	struct sigaction	no_act;
@@ -35,13 +35,14 @@ void	ft_handle_signals(void)
 	act.sa_handler = &ft_handle_sigint;
 	act.sa_flags = 0;
 	if (sigemptyset(&(act.sa_mask)) == -1)
-		perror("act sigemptyset");
+		return (ft_perror("act sigemptyset"), 0);
 	if (sigaction(SIGINT, &act, NULL) == -1)
-		perror("SIGINT sigaction");
+		return (ft_perror("SIGINT sigaction"), 0);
 	no_act.sa_handler = SIG_IGN;
 	no_act.sa_flags = 0;
 	if (sigemptyset(&(no_act.sa_mask)) == -1)
-		perror("no_act sigemptyset");
+		return (ft_perror("no_act sigemptyset"), 0);
 	if (sigaction(SIGQUIT, &no_act, NULL) == -1)
-		perror("SIGQUIT sigaction");
+		return (ft_perror("SIGQUIT sigaction"), 0);
+	return (1);
 }
