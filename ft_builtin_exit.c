c3c7a3c07a9899b6ebd2a1fb41a62076a29a6f20
Abdel Benamara <abdelb.contact@gmail.com>
2023-08-30 13:55:35 +0200

    Updated ft_builtin_exit

--- ft_builtin_exit.c
+++ ft_builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 22:38:20 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/12 01:14:36 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/08/30 13:42:38 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,19 @@ int	ft_builtin_exit(char **argv, t_list **env)
 	int	i;
 
 	if (!(argv[1]))
-		g_signum = SIGTERM;
-	else
+		return (g_signum = SIGTERM, ft_env_geti(env, "?"));
+	i = (argv[1][0] == '-');
+	while (argv[1][i])
 	{
-		i = (argv[1][0] == '-');
-		while (argv[1][i])
-		{
-			if (!ft_isdigit(argv[1][i]))
-				return (ft_pstderr3("exit", \
-					argv[1], "numeric argument required"), 2);
-			++i;
-		}
-		if (argv[1] && argv[2])
-			return (ft_pstderr2("exit", "too many arguments"), EXIT_FAILURE);
-		g_signum = SIGTERM;
-		i = ft_atoi(argv[1]) % 256;
-		if (i < 0)
-			return (i + 256);
-		return (i);
+		if (!ft_isdigit(argv[1][i]))
+			return (g_signum = SIGTERM, ft_pstderr3("exit", \
+				argv[1], "numeric argument required"), 2);
+		++i;
 	}
-	return (ft_env_geti(env, "?"));
+	if (argv[1] && argv[2])
+		return (ft_pstderr2("exit", "too many arguments"), EXIT_FAILURE);
+	i = ft_atoi(argv[1]) % 256;
+	if (i < 0)
+		i += 256;
+	return (g_signum = SIGTERM, i);
 }
