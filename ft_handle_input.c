ac26a07fe19db4ef548c26aa1044e3214e7272a3
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-02 17:54:48 +0200

    Updated ft_handle_input

--- ft_handle_input.c
+++ ft_handle_input.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 17:51:16 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/11 19:36:24 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/01 18:21:21 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ft_handle_input(t_list **cmds, int *writefd)
 {
-	const uint8_t	here_doc = !ft_strncmp((*cmds)->content, "<<", 3);
+	const uint8_t	hdoc = (*cmds) && !ft_strncmp((*cmds)->content, "<<", 3);
 
-	if (!here_doc && ft_strncmp((*cmds)->content, "<", 2))
+	if (!(*cmds) || (!hdoc && ft_strncmp((*cmds)->content, "<", 2)))
 		return (EXIT_SUCCESS);
 	if (close(writefd[1]) == -1)
 		return (ft_perror("close"), -1);
@@ -27,7 +27,7 @@ int	ft_handle_input(t_list **cmds, int *writefd)
 	ft_lst_pop(cmds, &free);
 	if (!(*cmds) || !ft_strncmp((*cmds)->content, "|", 2))
 		return (ft_pstderr("syntax error near unexpected token `newline'"), -2);
-	if (here_doc)
+	if (hdoc)
 	{
 		if (!ft_here_document((*cmds)->content, writefd))
 			return (ft_lstclear(cmds, &free), -1);
@@ -38,5 +38,5 @@ int	ft_handle_input(t_list **cmds, int *writefd)
 			ft_lst_pop(cmds, &free);
 		return (ft_lst_pop(cmds, &free), -1);
 	}
-	return (ft_lst_pop(cmds, &free), EXIT_SUCCESS);
+	return (ft_lst_pop(cmds, &free), ft_handle_input(cmds, writefd));
 }
