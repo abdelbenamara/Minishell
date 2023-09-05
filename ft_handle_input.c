ab22e56996864bbd7e1928b00f2e38058f55a994
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-05 22:05:59 +0200

    Updated ft_handle_input

--- ft_handle_input.c
+++ ft_handle_input.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 17:51:16 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/05 14:36:09 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/05 21:16:56 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,6 @@ int	ft_handle_input(t_list **cmds, int *writefd)
 	if (pipe(writefd) == -1)
 		return (ft_perror("pipe"), -1);
 	ft_lst_pop(cmds, &free);
-	if (!(*cmds) || !ft_strncmp((*cmds)->content, "|", 2))
-		return (ft_pstderr("syntax error near unexpected token `|'"), -2);
 	if (hdoc)
 	{
 		if (!ft_here_document((*cmds)->content, writefd))
