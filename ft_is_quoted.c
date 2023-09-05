5aabc23377a4bec9875caeef206e062d5c52f26b
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-05 22:05:30 +0200

    Updated ft_is_quoted

--- ft_is_quoted.c
+++ ft_is_quoted.c
@@ -6,22 +6,22 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 20:18:38 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/02 20:47:44 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/05 18:33:08 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-size_t	ft_is_quoted(char *str, char c)
+size_t	ft_is_quoted(char *str)
 {
 	size_t	i;
 	size_t	j;
 
 	i = 0;
 	j = 1;
-	while (str[i + j] && str[i + j] != c)
+	while (str[i + j] && str[i + j] != str[0])
 		++j;
-	if (str[i + j] == c)
+	if (str[i + j] == str[0])
 		i += j;
 	return (i);
 }
