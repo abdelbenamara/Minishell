8bce2768acfc863ab7743b4019856817f783ea9e
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-08 00:10:23 +0200

    Updated ft_builtin_echo

--- ft_builtin_echo.c
+++ ft_builtin_echo.c
@@ -6,31 +6,47 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 12:55:04 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/09 18:30:22 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/07 23:44:54 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static uint8_t	ft_no_new_line(char *str)
+{
+	size_t	i;
+
+	if (ft_strncmp(str, "-n", 2))
+		return (0);
+	i = 2;
+	while (str[i] && str[i] == 'n')
+		++i;
+	if (str[i])
+		return (0);
+	return (1);
+}
+
 int	ft_builtin_echo(char **argv, t_list **env)
 {
 	uint8_t	trailing_newline;
 	size_t	i;
+	size_t	j;
 
 	(void) env;
 	trailing_newline = 1;
 	i = 1;
-	if (argv[1] && !ft_strncmp(argv[1], "-n", 3))
+	while (argv[i] && ft_no_new_line(argv[i]))
 	{
 		trailing_newline = 0;
 		++i;
 	}
-	while (argv[i])
+	j = 0;
+	while (argv[i + j])
 	{
-		if ((trailing_newline && i > 1) || (!trailing_newline && i > 2))
+		if (j > 0)
 			ft_printf(" ");
-		ft_printf("%s", argv[i]);
-		++i;
+		ft_printf("%s", argv[i + j]);
+		++j;
 	}
 	if (trailing_newline)
 		ft_printf("\n");
