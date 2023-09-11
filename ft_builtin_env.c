29e139dee0cf4e0e3dca64f1b6af5d1ceb0e47f4
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-11 20:13:37 +0200

    Updated ft_builtin_env

--- ft_builtin_env.c
+++ ft_builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 01:11:01 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/11 13:49:58 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/11 18:24:12 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,7 @@ int	ft_builtin_env(char **argv, t_list **env)
 	lst = *env;
 	while (lst)
 	{
-		if (ft_strncmp(lst->content, "?=", 2)
-			&& ft_strncmp(lst->content, "!line=", 6))
+		if (ft_strncmp(lst->content, "?=", 2))
 		{
 			if (*(ft_strchr(((char *) lst->content), '=') + 1))
 				ft_printf("%s\n", lst->content);
