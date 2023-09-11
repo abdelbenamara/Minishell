20f9c6ed4e50361e9f62e138b708e9401c7958a8
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-11 15:21:38 +0200

    Updated ft_env_to_tab

--- ft_env_to_tab.c
+++ ft_env_to_tab.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 21:31:01 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/10 14:29:15 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/11 13:49:51 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,14 @@ char	**ft_env_to_tab(t_list *lst)
 	char	**tab;
 	size_t	i;
 
-	tab = malloc(ft_lstsize(lst) * sizeof(char *));
+	tab = malloc((ft_lstsize(lst) - 1) * sizeof(char *));
 	if (!tab)
 		return (NULL);
 	i = 0;
 	while (lst)
 	{
-		if (ft_strncmp(lst->content, "?=", 2))
+		if (ft_strncmp(lst->content, "?=", 2)
+			&& ft_strncmp(lst->content, "!line=", 6))
 		{
 			tab[i] = ft_strdup(lst->content);
 			++i;
