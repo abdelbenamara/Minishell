4045eef3e2992d26fb9b71a298eaa8738a4a18ed
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-11 22:56:14 +0200

    Updated ft_prc_del

--- ft_prc_del.c
+++ ft_prc_del.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 18:06:25 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/10 14:42:31 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/11 22:13:27 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	ft_prc_del(void *proc)
 {
-	if (((t_proc *) proc)->readfd)
+	if (((t_proc *) proc)->readfd[0])
 		close(((t_proc *) proc)->readfd[0]);
-	if (((t_proc *) proc)->writefd)
+	if (((t_proc *) proc)->writefd[1])
 		close(((t_proc *) proc)->writefd[1]);
 	free(proc);
 }
