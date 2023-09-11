b0b1ca77896f7e9e5542af243ea8c27d8100ca45
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-11 22:56:12 +0200

    Updated ft_prc_new

--- ft_prc_new.c
+++ ft_prc_new.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 22:26:33 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/10 22:27:33 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/11 22:29:09 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ t_proc	*ft_prc_new(void)
 	if (!proc)
 		return (NULL);
 	proc->pid = -1;
-	proc->readfd = NULL;
-	proc->writefd = NULL;
+	proc->readfd[1] = 0;
+	proc->readfd[0] = 0;
+	proc->writefd[0] = 0;
+	proc->writefd[1] = 0;
 	return (proc);
 }
