3423301c7fdda066e28e3f5867dd739b6dd9bbf0
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-13 05:17:22 +0200

    Updated ft_tkn_count

--- ft_tkn_count.c
+++ ft_tkn_count.c
@@ -6,13 +6,13 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 00:56:23 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/11 09:28:23 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/13 03:13:52 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-size_t	ft_tkn_count(t_list *lst, char *str)
+size_t	ft_tkn_count(t_list *lst, char *str, uint8_t all)
 {
 	const size_t	n = ft_strlen(str) + 1;
 	size_t			count;
@@ -22,6 +22,8 @@ size_t	ft_tkn_count(t_list *lst, char *str)
 	{
 		if (lst->content && !ft_strncmp(lst->content, str, n))
 			++count;
+		if (!all && !ft_strncmp(lst->content, "|", 2))
+			break ;
 		lst = lst->next;
 	}
 	return (count);
