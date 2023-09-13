04a5ec1c705b79be2cb22c71352b71d36904866c
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-13 05:33:43 +0200

    Updated ft_command_split

--- ft_command_split.c
+++ ft_command_split.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 23:10:30 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/10 14:33:36 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/13 01:50:30 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static char	*ft_command_expand(char *cmd, t_list **env, char c)
 	char	*str;
 
 	tmp = ft_expand(cmd, env, 0, 0);
-	str = ft_command_setup(tmp, c, 0);
+	str = ft_command_setup(tmp, c, '\n', 0);
 	free(tmp);
 	return (str);
 }
@@ -32,7 +32,7 @@ char	**ft_command_split(char *cmd, t_list **env, char c, uint8_t expand)
 	if (expand)
 		str = ft_command_expand(cmd, env, c);
 	else
-		str = ft_command_setup(cmd, c, 1);
+		str = ft_command_setup(cmd, c, '\n', 1);
 	argv = ft_split(str, c);
 	if (!argv)
 		return (free(str), NULL);
