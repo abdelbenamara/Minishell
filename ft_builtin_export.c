cee1b46b527224ad4360f9d9632c57adf5433ed3
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-13 05:35:17 +0200

    Updated ft_builtin_export

--- ft_builtin_export.c
+++ ft_builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 02:22:42 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/11 18:24:19 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/13 05:20:12 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,55 +66,71 @@ static void	ft_print_variables(t_list **env)
 	ft_tab_free(envp);
 }
 
-static uint8_t	ft_check_identifier(char *str)
+static uint8_t	ft_check_identifier(char *str, uint8_t silent)
 {
 	size_t	i;
 
 	if (!ft_isalpha(str[0]) && str[0] != '_')
+	{
+		if (!silent)
+			ft_dprintf(STDERR_FILENO, \
+				"export: `%s': not a valid identifier\n", str);
 		return (0);
+	}
 	i = 0;
 	while (str[i] && (ft_isalnum(str[i]) || str[i] == '_'))
 		++i;
 	if (str[i] && str[i] != '=')
+	{
+		if (!silent)
+			ft_dprintf(STDERR_FILENO, \
+				"export: `%s': not a valid identifier\n", str);
 		return (0);
+	}
 	return (1);
 }
 
-static void	ft_export_variable(char *var, t_list **env)
-{
-	char	*str;
-
-	str = ft_strchr(var, '=');
-	if (str)
-	{
-		*str = '\0';
-		str += 1;
-	}
-	ft_env_puts(env, var, str);
-}
-
-int	ft_builtin_export(char **argv, t_list **env)
+static int	ft_export_variables(char **argv, t_list **env, uint8_t silent)
 {
 	int		code;
 	size_t	i;
+	char	*str;
 
-	if (!(argv[1]))
-		return (ft_print_variables(env), EXIT_SUCCESS);
-	if (!ft_strncmp(argv[1], "-", 1))
-		return (ft_pstderr3("export", argv[1], "invalid option"), 2);
 	code = EXIT_SUCCESS;
 	i = 1;
 	while (argv[i])
 	{
-		if (!ft_check_identifier(argv[i]))
-		{
-			ft_dprintf(STDERR_FILENO, \
-				"export: `%s': not a valid identifier\n", argv[i]);
+		if (!ft_check_identifier(argv[i], silent))
 			code = EXIT_FAILURE;
-		}
 		else
-			ft_export_variable(argv[i], env);
+		{
+			str = ft_strchr(argv[i], '=');
+			if (str)
+			{
+				*str = '\0';
+				str += 1;
+			}
+			ft_env_puts(env, argv[i], str);
+		}
 		++i;
 	}
 	return (code);
+}
+
+int	ft_builtin_export(char *cmd, char **argv, t_list **env, uint8_t silent)
+{
+	(void) cmd;
+	if (!(argv[1]))
+	{
+		if (!silent)
+			ft_print_variables(env);
+		return (EXIT_SUCCESS);
+	}
+	if (!ft_strncmp(argv[1], "-", 1))
+	{
+		if (!silent)
+			ft_pstderr3("export", argv[1], "invalid option");
+		return (2);
+	}
+	return (ft_export_variables(argv, env, silent));
 }
