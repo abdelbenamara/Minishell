fc3d15af249ac41614277ed60d16399c4cfd7881
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-13 05:33:30 +0200

    Updated ft_is_quoted

--- ft_is_quoted.c
+++ ft_is_quoted.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 20:18:38 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/08 09:53:50 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/13 04:50:07 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,13 @@
 size_t	ft_is_quoted(char *str)
 {
 	size_t	i;
-	size_t	j;
 
 	if (str[0] != '\'' && str[0] != '"')
 		return (0);
-	i = 0;
-	j = 1;
-	while (str[i + j] && str[i + j] != str[0])
-		++j;
-	if (str[i + j] == str[0])
-		i += j;
-	return (i);
+	i = 1;
+	while (str[i] && str[i] != str[0])
+		++i;
+	if (str[i] == str[0])
+		return (i);
+	return (0);
 }
