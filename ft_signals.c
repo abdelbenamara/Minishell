552e7d2d2c0b80080e569d25cc88394f3b2a7867
Anes BENAMARA <abenamar@student.42.fr>
2023-09-13 17:21:50 +0200

    Updated ft_signals

--- ft_signals.c
+++ ft_signals.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 17:11:43 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/13 04:40:12 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/13 16:51:30 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ static void	ft_sigint(int signum)
 {
 	g_signum = signum;
 	if (waitpid(-1, NULL, WUNTRACED) != -1)
-		ft_printf("\n");
+		printf("\n");
 	while (waitpid(-1, NULL, WUNTRACED) != -1)
 		;
 	if (errno != ECHILD)
-		ft_perror("wait");
+		ft_perror("wait: ");
 	rl_replace_line("", 1);
 	rl_done = 1;
 }
@@ -41,14 +41,14 @@ uint8_t	ft_signals(void)
 	usr_act.sa_handler = &ft_sigint;
 	usr_act.sa_flags = 0;
 	if (sigemptyset(&(usr_act.sa_mask)) == -1)
-		return (ft_perror("usr_act sigemptyset"), 0);
+		return (ft_perror("usr_act sigemptyset: "), 0);
 	if (sigaction(SIGINT, &usr_act, NULL) == -1)
-		return (ft_perror("SIGINT sigaction"), 0);
+		return (ft_perror("SIGINT sigaction: "), 0);
 	ign_act.sa_handler = SIG_IGN;
 	ign_act.sa_flags = 0;
 	if (sigemptyset(&(ign_act.sa_mask)) == -1)
-		return (ft_perror("ign_act sigemptyset"), 0);
+		return (ft_perror("ign_act sigemptyset: "), 0);
 	if (sigaction(SIGQUIT, &ign_act, NULL) == -1)
-		return (ft_perror("SIGQUIT sigaction"), 0);
+		return (ft_perror("SIGQUIT sigaction: "), 0);
 	return (1);
 }
