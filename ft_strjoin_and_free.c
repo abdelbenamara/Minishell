be733d9e1ec97628b26c0cdbc7422a0ce48ec96a
Anes BENAMARA <abenamar@student.42.fr>
2023-09-13 15:42:36 +0200

    Added ft_strjoin_and_free

---
+++ ft_strjoin_and_free.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin_and_free.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 15:18:39 by abenamar          #+#    #+#             */
+/*   Updated: 2023/09/13 15:20:00 by abenamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*ft_strjoin_and_free(char *s1, char *s2)
+{
+	char	*str;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (s2);
+	if (!s2)
+		return (s1);
+	str = ft_strjoin(s1, s2);
+	free(s1);
+	free(s2);
+	return (str);
+}
