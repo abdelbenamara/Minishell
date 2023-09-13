c4b9e7ee1e5e5afec10ec8bb4dcd51a8d2329303
Anes BENAMARA <abenamar@student.42.fr>
2023-09-13 15:42:48 +0200

    Updated ft_pstderr3

--- ft_pstderr3.c
+++ ft_pstderr3.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 17:43:54 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/11 14:16:07 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/13 15:39:39 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 int	ft_pstderr3(char *s1, char *s2, char *s3)
 {
-	return (ft_dprintf(STDERR_FILENO, "minishell: %s: %s: %s\n", s1, s2, s3));
+	char	*str;
+	int		n;
+
+	str = ft_strdup("minishell: ");
+	if (s1)
+		str = ft_strjoin_and_free(str, ft_strdup(s1));
+	if (s2)
+		str = ft_strjoin_and_free(str, ft_strdup(s2));
+	if (s3)
+		str = ft_strjoin_and_free(str, ft_strdup(s3));
+	str = ft_strjoin_and_free(str, ft_strdup("\n"));
+	n = ft_strlen(str);
+	write(STDERR_FILENO, str, n * sizeof(char));
+	free(str);
+	return (n);
 }
