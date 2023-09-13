0ba0a2f14d363209ed336002d56bb54f0b264471
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-13 05:33:38 +0200

    Updated ft_command_setup

--- ft_command_setup.c
+++ ft_command_setup.c
@@ -6,44 +6,41 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 17:19:43 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/10 11:45:32 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/13 05:07:54 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static size_t	ft_save_character(char *str, char c)
+static size_t	ft_save_character(char *str, char c1, char c2)
 {
 	size_t	i;
 
 	i = 1;
 	while (str[i] != str[0])
 	{
-		if (str[i] == c)
-			str[i] = '\n';
+		if (str[i] == c1)
+			str[i] = c2;
 		++i;
 	}
 	return (i);
 }
 
-static size_t	ft_handle_quoted_sequence(char *str, char c, uint8_t quote)
+static size_t	ft_handle_quotes(char *str, char c1, char c2, uint8_t quote)
 {
-	char	q;
 	size_t	i;
 	size_t	j;
 
-	i = ft_save_character(str, c);
+	i = ft_save_character(str, c1, c2);
 	if (quote)
-		return (i);
-	q = str[0];
-	str[0] = str[1];
-	i = 0;
-	while (str[i + 1] != q)
+		return (i + 1);
+	i -= 1;
+	j = 0;
+	while (j < i)
 	{
-		str[i] = str[i + 1];
-		++i;
+		str[j] = str[j + 1];
+		++j;
 	}
-	j = i;
 	while (str[j] && str[j + 1])
 	{
 		str[j] = str[j + 2];
@@ -53,11 +50,13 @@ static size_t	ft_handle_quoted_sequence(char *str, char c, uint8_t quote)
 	return (i);
 }
 
-char	*ft_command_setup(char *cmd, char c, uint8_t quote)
+char	*ft_command_setup(char *cmd, char c1, char c2, uint8_t quote)
 {
 	char	*str;
 	size_t	i;
 
+	if (!cmd)
+		return (NULL);
 	str = ft_strdup(cmd);
 	if (!str)
 		return (NULL);
@@ -67,10 +66,11 @@ char	*ft_command_setup(char *cmd, char c, uint8_t quote)
 		if (str[i] == '\t')
 			str[i] = ' ';
 		else if (str[i] == '"' && ft_is_quoted(str + i))
-			i += ft_handle_quoted_sequence(str + i, c, quote);
+			i += ft_handle_quotes(str + i, c1, c2, quote);
 		else if (str[i] == '\'' && ft_is_quoted(str + i))
-			i += ft_handle_quoted_sequence(str + i, c, quote);
-		++i;
+			i += ft_handle_quotes(str + i, c1, c2, quote);
+		else
+			++i;
 	}
 	return (str);
 }
