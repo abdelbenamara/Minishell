7df892773de3d1f360b148797860030ca041d6f3
Anes BENAMARA <abenamar@student.42.fr>
2023-09-13 15:42:50 +0200

    Updated ft_pstderr2

--- ft_pstderr2.c
+++ ft_pstderr2.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 17:42:15 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/11 14:16:04 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/13 15:25:56 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 int	ft_pstderr2(char *s1, char *s2)
 {
-	return (ft_dprintf(STDERR_FILENO, "minishell: %s: %s\n", s1, s2));
+	return (ft_pstderr3(s1, s2, NULL));
 }
