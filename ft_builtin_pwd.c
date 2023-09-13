7d39721b8598e4e1075963bcf8eecabb939bb999
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-13 05:35:21 +0200

    Updated ft_builtin_pwd

--- ft_builtin_pwd.c
+++ ft_builtin_pwd.c
@@ -6,23 +6,27 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 13:06:00 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/11 00:44:24 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/12 23:31:57 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_builtin_pwd(char **argv, t_list **env)
+int	ft_builtin_pwd(char *cmd, char **argv, t_list **env, uint8_t silent)
 {
 	char	*cwd;
 
-	(void) env;
+	((void) cmd, (void) env);
 	if (argv[1] && !ft_strncmp(argv[1], "-", 1))
-		return (ft_dprintf(STDERR_FILENO, \
-			"pwd: %s: invalid option\n", argv[1]), 2);
+	{
+		if (!silent)
+			ft_dprintf(STDERR_FILENO, "pwd: %s: invalid option\n", argv[1]);
+		return (2);
+	}
 	cwd = getcwd(NULL, 0);
 	if (!cwd)
-		return (ft_perror("pwd"), EXIT_FAILURE);
-	ft_printf("%s\n", cwd);
+		return (ft_perror("getcwd"), EXIT_FAILURE);
+	if (!silent)
+		ft_printf("%s\n", cwd);
 	return (free(cwd), EXIT_SUCCESS);
 }
