40d3583d61aa754286af4edcfc52f2dd5df09ec4
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-13 05:35:29 +0200

    Updated ft_builtin

--- ft_builtin.c
+++ ft_builtin.c
@@ -6,18 +6,18 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 22:47:49 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/12 09:21:22 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/12 23:18:30 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_builtin(char *cmd, t_list **env, uint8_t fake)
+int	ft_builtin(char *cmd, t_list **env, uint8_t silent)
 {
 	static char	*builtin[] = {
 		"echo", "cd", "pwd", "export", "unset", "env", "exit", NULL
 	};
-	static int	(*ft_builtin[])(char **, t_list **) = {
+	static int	(*ft_builtin[])(char *, char **, t_list **, uint8_t) = {
 		ft_builtin_echo, ft_builtin_cd, ft_builtin_pwd, ft_builtin_export,
 		ft_builtin_unset, ft_builtin_env, ft_builtin_exit
 	};
@@ -31,11 +31,8 @@ int	ft_builtin(char *cmd, t_list **env, uint8_t fake)
 	while (argv[0] && builtin[i])
 	{
 		if (!ft_strncmp(builtin[i], argv[0], 7))
-		{
-			if (!fake)
-				return (i = ft_builtin[i](argv, env), ft_tab_free(argv), i);
-			return (ft_tab_free(argv), EXIT_SUCCESS);
-		}
+			return (i = ft_builtin[i](cmd, argv, env, silent), \
+				ft_tab_free(argv), i);
 		++i;
 	}
 	return (ft_tab_free(argv), 127);
