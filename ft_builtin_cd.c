971f2ba5271af4cd5711072f42f1e9ec4ee46294
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-13 05:34:54 +0200

    Updated ft_builtin_cd

--- ft_builtin_cd.c
+++ ft_builtin_cd.c
@@ -6,35 +6,79 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 04:46:35 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/11 18:34:54 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/12 23:31:39 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_builtin_cd(char **argv, t_list **env)
+static char	*ft_check_for_empty_argument(char *cmd)
+{
+	char	**argv;
+
+	argv = ft_command_split(cmd, NULL, ' ', 0);
+	if (!argv || !(argv[1]))
+		return (NULL);
+	if (!ft_strncmp(argv[1], "\"\"", 3) || !ft_strncmp(argv[1], "''", 3))
+		return ("");
+	return (NULL);
+}
+
+static char	*ft_handle_home(t_list **env, uint8_t silent)
+{
+	char	*cwd;
+
+	cwd = ft_env_gets(env, "HOME");
+	if (!cwd)
+	{
+		if (!silent)
+			ft_pstderr2("cd", "HOME not set");
+		return (NULL);
+	}
+	return (cwd);
+}
+
+static char	*ft_handle_minus(t_list **env, uint8_t silent)
+{
+	char	*cwd;
+
+	cwd = ft_env_gets(env, "OLDPWD");
+	if (!cwd || !(*cwd))
+	{
+		if (!silent)
+			ft_pstderr2("cd", "OLDPWD not set");
+		return (NULL);
+	}
+	if (!silent)
+		ft_printf("%s\n", cwd);
+	return (cwd);
+}
+
+int	ft_builtin_cd(char *cmd, char **argv, t_list **env, uint8_t silent)
 {
 	char	*cwd;
 
 	if (argv[1] && argv[2])
-		return (ft_pstderr2("cd", "too many arguments"), EXIT_FAILURE);
+	{
+		if (!silent)
+			ft_pstderr2("cd", "too many arguments");
+		return (EXIT_FAILURE);
+	}
 	cwd = argv[1];
 	if (!cwd)
-		cwd = ft_env_gets(env, "HOME");
+		cwd = ft_check_for_empty_argument(cmd);
 	if (!cwd)
-		return (ft_pstderr2("cd", "HOME not set"), EXIT_FAILURE);
-	if (!ft_strncmp(cwd, "-", 2))
-	{
-		cwd = ft_env_gets(env, "OLDPWD");
-		if (!cwd || !(*cwd))
-			return (ft_pstderr2("cd", "OLDPWD not set"), EXIT_FAILURE);
-		ft_printf("%s\n", cwd);
-	}
+		cwd = ft_handle_home(env, silent);
+	if (cwd && !ft_strncmp(cwd, "-", 2))
+		cwd = ft_handle_minus(env, silent);
+	if (!cwd)
+		return (EXIT_FAILURE);
 	if (chdir(cwd) == -1)
 		return (ft_perror2("cd", cwd), EXIT_FAILURE);
 	cwd = getcwd(NULL, 0);
+	if (!cwd)
+		return (ft_perror("getcwd"), EXIT_FAILURE);
 	ft_env_puts(env, "OLDPWD", ft_env_gets(env, "PWD"));
 	ft_env_puts(env, "PWD", cwd);
-	free(cwd);
-	return (EXIT_SUCCESS);
+	return (free(cwd), EXIT_SUCCESS);
 }
