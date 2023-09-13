146d4d3b9c7a12033b48986b767a8ea2a37146e6
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-13 05:17:39 +0200

    Updated ft_prc_del

--- ft_prc_del.c
+++ ft_prc_del.c
@@ -6,17 +6,17 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 18:06:25 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/11 22:13:27 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/13 00:50:12 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_prc_del(void *proc)
+void	ft_prc_del(void *prc)
 {
-	if (((t_proc *) proc)->readfd[0])
-		close(((t_proc *) proc)->readfd[0]);
-	if (((t_proc *) proc)->writefd[1])
-		close(((t_proc *) proc)->writefd[1]);
-	free(proc);
+	if (((t_proc *) prc)->readfd[0])
+		close(((t_proc *) prc)->readfd[0]);
+	if (((t_proc *) prc)->writefd[1])
+		close(((t_proc *) prc)->writefd[1]);
+	free(prc);
 }
