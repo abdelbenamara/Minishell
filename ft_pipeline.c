8ccfab3885968b879b9b7dcc9e2eb2e7b5bcc684
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-27 15:58:02 +0200

    Updated ft_pipeline

--- ft_pipeline.c
+++ ft_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 18:02:17 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/13 16:50:52 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/27 14:06:01 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,27 +65,30 @@ static uint8_t	ft_fork(t_list **prcs, t_list **tkns, t_list **env)
 	return (((t_proc *)(*prcs)->content)->pid = cpid, 1);
 }
 
-int	ft_wait(t_list **prcs, int code)
+int	ft_wait(t_list *prcs, int code)
 {
 	pid_t	cpid;
 	int		wstatus;
 
-	cpid = ((t_proc *)(*prcs)->content)->pid;
-	ft_lstclear(prcs, &ft_prc_del);
+	if (!prcs)
+		return (code);
+	cpid = ((t_proc *) prcs->content)->pid;
+	(ft_wait(prcs->next, code), ft_lstdelone(prcs, &ft_prc_del));
+	if (cpid == -1)
+		return (code);
 	if (waitpid(cpid, &wstatus, WUNTRACED) == -1)
+	{
+		if (g_signum == SIGINT)
+			return (waitpid(cpid, NULL, WUNTRACED), printf("\n"), 130);
 		return (EXIT_FAILURE);
-	while (waitpid(-1, NULL, WUNTRACED) != -1)
-		;
-	if (errno != ECHILD)
-		return (ft_perror("wait: "), EXIT_FAILURE);
+	}
 	if (code != EXIT_SUCCESS)
 		return (code);
 	if (WIFSIGNALED(wstatus))
 	{
 		g_signum = WTERMSIG(wstatus);
 		if (g_signum == SIGQUIT)
-			printf("Quit");
-		printf("\n");
+			printf("Quit\n");
 		return (128 + g_signum);
 	}
 	return (WEXITSTATUS(wstatus));
@@ -114,15 +117,15 @@ int	ft_pipeline(t_list **tkns, t_list **env)
 	if (!ft_tkn_count(*tkns, "|", 1))
 	{
 		if (!ft_fork(&prcs, tkns, env))
-			return (ft_lstclear(tkns, &free), ft_wait(&prcs, EXIT_FAILURE));
-		return (ft_wait(&prcs, EXIT_SUCCESS));
+			return (ft_lstclear(tkns, &free), ft_wait(prcs, EXIT_FAILURE));
+		return (ft_wait(prcs, EXIT_SUCCESS));
 	}
 	while (*tkns)
 	{
 		if (!ft_next_pipe(&prcs, tkns))
-			return (ft_lstclear(tkns, &free), ft_wait(&prcs, EXIT_FAILURE));
+			return (ft_lstclear(tkns, &free), ft_wait(prcs, EXIT_FAILURE));
 		if (!ft_fork(&prcs, tkns, env))
-			return (ft_lstclear(tkns, &free), ft_wait(&prcs, EXIT_FAILURE));
+			return (ft_lstclear(tkns, &free), ft_wait(prcs, EXIT_FAILURE));
 		while (*tkns && ft_strncmp((*tkns)->content, "|", 2))
 		{
 			if (!ft_strncmp((*tkns)->content, "<<", 3))
@@ -131,5 +134,5 @@ int	ft_pipeline(t_list **tkns, t_list **env)
 		}
 		ft_lst_pop(tkns, &free);
 	}
-	return (ft_wait(&prcs, EXIT_SUCCESS));
+	return (ft_wait(prcs, EXIT_SUCCESS));
 }
