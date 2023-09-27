05dedb91c23641dac2f7974147a956c8b529493b
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-27 15:57:19 +0200

    Updated ft_env_puts

--- ft_env_puts.c
+++ ft_env_puts.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 04:04:45 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/09 18:12:09 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/27 14:14:53 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	ft_env_puts(t_list **env, char *key, char *value)
 	if (var)
 		str = ft_strjoin(key, var);
 	else
-		str = ft_strjoin(key, "=");
+		str = ft_strdup(key);
 	free(var);
 	var = ft_env_gets(env, key);
 	if (var)
