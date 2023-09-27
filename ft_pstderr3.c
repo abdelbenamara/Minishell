dffdff85f5265844e2818ee952ffe9d6bce7d376
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-27 15:56:06 +0200

    Updated ft_pstderr3

--- ft_pstderr3.c
+++ ft_pstderr3.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 17:43:54 by abenamar          #+#    #+#             */
-/*   Updated: 2023/09/13 16:41:16 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/15 10:56:14 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,5 @@
 
 int	ft_pstderr3(char *s1, char *s2, char *s3)
 {
-	char	*str;
-	int		n;
-
-	str = ft_strdup("minishell: ");
-	if (s1)
-		str = ft_strjoin_and_free(str, ft_strdup(s1));
-	if (s2)
-		str = ft_strjoin_and_free(str, ft_strdup(s2));
-	if (s3)
-		str = ft_strjoin_and_free(str, ft_strdup(s3));
-	str = ft_strjoin_and_free(str, ft_strdup("\n"));
-	n = ft_strlen(str);
-	if (write(STDERR_FILENO, str, n * sizeof(char)) == -1)
-		return (-1);
-	free(str);
-	return (n);
+	return (ft_pstderr4(s1, s2, s3, NULL));
 }
