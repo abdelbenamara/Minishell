78a3045dc08e652b0b2fba0e3d5535b61c881c2b
Abdel Benamara <abdelb.contact@gmail.com>
2023-09-27 15:57:48 +0200

    Updated ft_is_redirection

--- ft_is_redirection.c
+++ ft_is_redirection.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 18:34:21 by abenamar          #+#    #+#             */
-/*   Updated: 2023/08/09 18:34:39 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/09/15 10:35:58 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,5 @@
 uint8_t	ft_is_redirection(char *cmd)
 {
 	return (!ft_strncmp(cmd, "<<", 3) || !ft_strncmp(cmd, "<", 2)
-		|| !ft_strncmp(cmd, "|", 2)
 		|| !ft_strncmp(cmd, ">>", 3) || !ft_strncmp(cmd, ">", 2));
 }
